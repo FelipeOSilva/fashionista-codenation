5e1aff1047d7d94a388aedd8ba60959022b64944
Felipe Silva <felipe_96sp@msn.com>
2020-06-15 11:07:08 -0300

    Create handle Show/Hide Cart and Search page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,24 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Store } from "../../types";
 import { useSelector } from "react-redux";
+import { FiSearch, FiShoppingBag } from "react-icons/fi";
+
+import "./styles.css";
+import Search from "../Search";
+import Cart from "../Cart";
 
 const Header = () => {
   const [totalProductsCart, setTotalProductsCart] = useState(0);
+  const [showCart, setShowCart] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
   const { productsCart } = useSelector((state: Store) => state);
+
+  function handleCart() {
+    setShowCart(!showCart);
+  }
+  function handleSearch() {
+    setShowSearch(!showSearch);
+  }
 
   useEffect(() => {
     let totalProducts = productsCart.reduce(
@@ -15,17 +29,26 @@ const Header = () => {
     setTotalProductsCart(totalProducts);
   }, [productsCart]);
   return (
-    <header className="header">
-      <div className="container">
-        <Link to="/" className="header__logo">
-          <span>FASHIONISTA</span>
-        </Link>
-        <div className="header__actions">
-          <span>PQ</span>
-          <span>CP {totalProductsCart}</span>
+    <>
+      <header className="header">
+        <div className="container">
+          <Link to="/">
+            <span className="header__logo">FASHIONISTA</span>
+          </Link>
+          <div className="header__actions">
+            <button className="action__search" onClick={handleSearch}>
+              <FiSearch />
+            </button>
+            <button className="action__cart" onClick={handleCart}>
+              <FiShoppingBag />
+              <sup className="bagde">{totalProductsCart}</sup>
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      {showCart && <Cart />}
+      {showSearch && <Search />}
+    </>
   );
 };
 
