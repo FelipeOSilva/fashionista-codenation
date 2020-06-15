004b4f31dd72eb7047b5b430e09df742540ebe36
Felipe Silva <felipe_96sp@msn.com>
2020-06-14 22:27:20 -0300

    Load API data into storage

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import DetailProduct from "./pages/DetailProduct";
+import { useDispatch } from "react-redux";
+import { setProductStorage } from "./services/api";
+import { setProducts } from "./store/actions";
 const Routes = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setProductStorage()
+      .then((products) => {
+        dispatch(setProducts(products));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(setProducts([]));
+      });
+  }, [dispatch]);
   return (
     <BrowserRouter>
       <Route component={Home} path="/" exact />
